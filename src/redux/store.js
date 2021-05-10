6b6686b62043a46ead3428e19866e14ec8c8aaa3
builtbyjosh <pc.refuge@gmail.com>
2021-05-09 23:30:24 -0500

    initial redux store setup

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import breweryReducer from "./reducers/breweryReducer";
+import reviewReducer from "./reducers/reviewReducer";
+
+const rootReducer = combineReducers({
+  brewery: breweryReducer,
+  reviewReducer: reviewReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
