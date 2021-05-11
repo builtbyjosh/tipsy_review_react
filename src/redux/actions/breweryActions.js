4dfb185d400b88182cfc87ce90794504f974b96a
builtbyjosh <pc.refuge@gmail.com>
2021-05-11 12:18:03 -0500

    Refactoring

--- src/redux/actions/breweryActions.js
+++ src/redux/actions/breweryActions.js
@@ -2,6 +2,6 @@ export const getBreweries = () => {
   return (dispatch) => {
     fetch("http://localhost:3001/breweries")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => dispatch({ type: "FETCH_API_BREWERIES", payload: data }));
   };
 };
--- src/redux/reducers/breweryReducer.js
+++ src/redux/reducers/breweryReducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "FETCH_API_BREWERIES":
       return action.payload;
+    case "CREATE_BREWERY":
+      return [...state, action.payload];
     default:
       return state;
   }
