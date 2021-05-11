efc1ee31e8df1ecd8f492fa5aca67b6b968682f4
builtbyjosh <pc.refuge@gmail.com>
2021-05-11 10:43:25 -0500

    created fetch action and refactored reducer

--- src/redux/actions/breweryApiAction.js
+++ src/redux/actions/breweryApiAction.js
@@ -2,8 +2,8 @@ export const getOpenBreweries = (query = "dog") => {
   return (dispatch) => {
     fetch(`https://api.openbrewerydb.org/breweries/search?query=${query}`)
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
+      .then((data) =>
+        dispatch({ type: "FETCH_API_BREWERIES", payload: data })
+      );
   };
 };
--- src/redux/reducers/breweryReducer.js
+++ src/redux/reducers/breweryReducer.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
   switch (action.type) {
+    case "FETCH_API_BREWERIES":
+      return action.payload;
     default:
       return state;
   }
