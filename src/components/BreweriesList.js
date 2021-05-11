a7705e3b7d9d8408d332f3485d9697b6e4053e2b
builtbyjosh <pc.refuge@gmail.com>
2021-05-11 10:44:03 -0500

    created component to show brewery and imported to list

--- src/components/BreweriesList.js
+++ src/components/BreweriesList.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
-import { getBreweries } from "../redux/actions/breweryActions";
-import {getOpenBreweries} from '../redux/actions/breweryApiAction'
+import { getOpenBreweries } from "../redux/actions/breweryApiAction";
 import { connect } from "react-redux";
+import BreweriesListItem from "./BreweriesListItem";
 
 class BreweriesList extends Component {
   componentDidMount() {
     this.props.getOpenBreweries();
   }
 
-  
-
   render() {
     return (
       <div>
         <h1>Brewery List</h1>
+        {this.props.brewery.map((b) => (
+          <BreweriesListItem brewery={b} />
+        ))}
       </div>
     );
   }
---
+++ src/components/BreweriesListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function BreweriesListItem({ brewery }) {
+  return (
+    <div>
+      <p>{brewery.name}</p>
+    </div>
+  );
+}
+
+// address_2: null
+// address_3: null
+// brewery_type: "micro"
+// city: "Oxford"
+// country: "United States"
+// county_province: null
+// created_at: "2018-07-24T00:00:00.000Z"
+// id: 9108
+// latitude: null
+// longitude: null
+// name: "Bog Turtle Brewery, LLC"
+// obdb_id: "bog-turtle-brewery-llc-oxford"
+// phone: "4847580416"
+// postal_code: "19363-1601"
+// state: "Pennsylvania"
+// street: "14 S 3rd St Ste 1"
+// updated_at: "2018-08-11T00:00:00.000Z"
+// website_url: "http://www.bogturtlebrewery.com"
