80d183174717ac64ca71034837f7d109686b3cf9
builtbyjosh <pc.refuge@gmail.com>
2021-05-11 13:51:53 -0500

    adding reviews to brewery details

--- src/components/BreweryDetails.js
+++ src/components/BreweryDetails.js
@@ -23,19 +23,35 @@ class BreweryDetails extends Component {
   }
 
   render() {
-    
+    const brewery = this.props.brewery;
+
+    // const renderReview = brewery.reviews.map((review) => {
+    //   <div>
+    //     <h3>{review.name}</h3>
+    //     <p>{review.comment}</p>
+    //   </div>
+    // })
+
     return (
       <div>
-        <p>{this.props.brewery.name}</p>
+        <p>{brewery.name}</p>
+        <p>{brewery.address}</p>
+        <p>{brewery.city}, {brewery.state}</p>
+
+        <p>Reviews</p>
+        
       </div>
     );
   }
+  
 }
 
 const mapStateToProps = (state) => {
   return {
-    brewery: state.brewery,
+    brewery: state.brewery,    
   };
+  debugger
 };
+
 
 export default connect(mapStateToProps, { getBrewery })(BreweryDetails);
