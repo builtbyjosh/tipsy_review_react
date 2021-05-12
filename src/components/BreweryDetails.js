d23d65603bc57f3bb5e7c03316b8eb9d9511acde
builtbyjosh <pc.refuge@gmail.com>
2021-05-11 19:19:40 -0500

    debugging displaying reviews

--- src/components/BreweryDetails.js
+++ src/components/BreweryDetails.js
@@ -1,57 +1,40 @@
-// import React from 'react';
-
-// debugger
-
-// const BreweryDetails = ({brewery}) => {
-//   return (
-//     <div>
-//       <p>{brewery.name}</p>
-//     </div>
-//   );
-// }
-
-// export default BreweryDetails;
-
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getBrewery } from "../redux/actions/breweryActions";
+import ReviewDetails from "./ReviewDetails";
 
 class BreweryDetails extends Component {
-  componentDidMount() {
+  componentDidMount = () => {
     this.props.getBrewery(this.props.match.params.id);
-  }
+  };
 
   render() {
     const brewery = this.props.brewery;
 
-    // const renderReview = brewery.reviews.map((review) => {
-    //   <div>
-    //     <h3>{review.name}</h3>
-    //     <p>{review.comment}</p>
-    //   </div>
-    // })
+    const renderReviews = this.props.brewery.reviews.map((review) => {
+      <ReviewDetails review={review} />;
+    });
 
     return (
       <div>
         <p>{brewery.name}</p>
         <p>{brewery.address}</p>
-        <p>{brewery.city}, {brewery.state}</p>
-
-        <p>Reviews</p>
-        
+        <p>
+          {brewery.city}, {brewery.state}
+        </p>
+        <div>
+          <p>Current Reviews:</p>
+          <div>{renderReviews}</div>
+        </div>
       </div>
     );
   }
-  
 }
 
 const mapStateToProps = (state) => {
   return {
-    brewery: state.brewery,    
+    brewery: state.brewery,
   };
-  debugger
 };
-
 
 export default connect(mapStateToProps, { getBrewery })(BreweryDetails);
