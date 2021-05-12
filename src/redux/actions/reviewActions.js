c7ccac7d5707feaa84afe8ccb26400770d117cb7
builtbyjosh <pc.refuge@gmail.com>
2021-05-12 11:21:35 -0500

    added get reviews action and reducer

--- src/redux/actions/reviewActions.js
+++ src/redux/actions/reviewActions.js
@@ -1,3 +1,11 @@
+export const getReviews = () => {
+  return (dispatch) => {
+    fetch("http://localhost:3001/reviews")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "FETCH_REVIEWS", payload: data }));
+  };
+};
+
 export const createOpenBrewery = (newReviewData) => {
   return (dispatch) => {
     fetch("http://localhost:3001/breweries/:id/reviews", {
--- src/redux/reducers/reviewReducer.js
+++ src/redux/reducers/reviewReducer.js
@@ -1,5 +1,9 @@
 export default (state = [], action) => {
   switch (action.type) {
+    case "FETCH_REVIEWS":
+      return action.payload
+    case "CREATE_REVIEWS":
+      return [...state, action.payload]
     default:
       return state;
   }
